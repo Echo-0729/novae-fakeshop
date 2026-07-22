6fc4061fb7d380ed83dfdf2ef739ae68823335a1
pearlynlee01-afk <pearlynlee01@gmail.com>
2026-07-22 17:38:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const menuButton = document.querySelector(".menu-button");
-const navigation = document.querySelector(".main-nav");
 const filter = document.querySelector("#filter");
 const productGrid = document.querySelector("#productGrid");
 
-menuButton.addEventListener("click", () => {
-  navigation.classList.toggle("open");
-});
-
 filter.addEventListener("change", () => {
   const selectedValue = filter.value;
-  const products = [...document.querySelectorAll(".card")];
+  const products = Array.from(document.querySelectorAll(".card")];
 
   if (selectedValue === "low-high") {
     products
-      .sort((a, b) => Number(a.dataset.price) - Number(b.dataset.price))
-      .forEach(product => productGrid.appendChild(product));
-
+      .sort((a, b) => 
+ {
+      return Number(a.dataset.price) - Number(b.dataset.price);
+    });
     products.forEach(product => {
-      product.style.display = "block";
+      product.style.display = "block";   
+      productGrid.appendChild(product);
     });
 
     return;
   }
 
   products.forEach(product => {
-    const category = product.dataset.category;
-    product.style.display =
-      category === selectedValue ? "block" : "none";
+    if (category === selectedValue) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
   });
 });
